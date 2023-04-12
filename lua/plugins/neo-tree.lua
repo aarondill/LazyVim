dc15b7e93bf97046c6032106e2f0278572896c7f
aarondill <aaronsacks2006@gmail.com>
2023-04-12 15:32:57 -0500

    change neo-tree to open with o

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,11 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    window = {
+      mappings = {
+        ["o"] = "open",
+      },
+    },
     filesystem = {
       filtered_items = {
         hide_dotfiles = false,
