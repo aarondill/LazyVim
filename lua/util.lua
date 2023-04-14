ff3abeb2a2a4fc54e2960f6e1f5cd52032c741a9
aarondill <aaronsacks2006@gmail.com>
2023-04-14 02:18:08 -0500

    improve keymap function and create util.bind function

--- lua/util.lua
+++ lua/util.lua
@@ -18,13 +18,20 @@ M.config_path = function()
 end
 
 -- A wrapper around |vim.keymap.set| which adds a desc arg and allows multiple lhs to be defined at the same time
----@param mode string|table    Mode short-name, see |nvim_set_keymap()|.
----@param lhs string|table     Left-hand side |{lhs}| of the mapping.
+---@param mode string|string[] Mode short-name, see |nvim_set_keymap()|.
+---@param lhs string|string[]  Left-hand side |{lhs}| of the mapping.
 ---@param rhs string|function  Right-hand side |{rhs}| of the mapping, can be a Lua function.
----@param desc string          Description
+---@param desc string?         Description or opts
 ---@param opts table?          Table of |:map-arguments|. (If desc is defined, will be overwritten by arg)
+---@overload fun(mode:string|string[], lhs:string|string[], rhs:string|function, opts:table)
 M.set_key_map = function(mode, lhs, rhs, desc, opts)
-  local descOpts = vim.tbl_extend("force", opts or {}, { desc = desc })
+  local descOpts
+  if type(desc) == "string" then
+    descOpts = vim.tbl_extend("force", opts or {}, { desc = desc })
+  else
+    assert((desc == nil or type(desc) == "table") and opts == nil, "Desc can only be a table if opts is nil")
+    descOpts = desc
+  end
 
   assert(type(lhs) == "table" or type(lhs) == "string", "lhs must be a string or table")
   local lhsT
@@ -36,6 +43,16 @@ M.set_key_map = function(mode, lhs, rhs, desc, opts)
 
   for _, l in pairs(lhsT) do
     vim.keymap.set(mode, l, rhs, descOpts)
+  end
+end
+
+M.bind = function(f, ...)
+  local args = { ... }
+  return function(...)
+    for _, v in ipairs({ ... }) do
+      table.insert(args, v)
+    end
+    return f(unpack(args))
   end
 end
 return M
