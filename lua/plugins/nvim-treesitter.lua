f9e94611577172caa5dec0fd7ebb61ef811ddbd8
aarondill <aaronsacks2006@gmail.com>
2023-04-14 02:26:38 -0500

    rename help.lua to nvim-treesitter.lua to more clearly show changes made

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    opts.ignore_install = { "help" }
+    opts.ignore_install = { "help" } -- Help --> vimdoc
 
     if type(opts.ensure_installed) == "table" then
       vim.list_extend(opts.ensure_installed, {
