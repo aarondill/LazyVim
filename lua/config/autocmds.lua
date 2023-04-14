20121db8a88e889ef500068be1c328c76bc41729
aarondill <aaronsacks2006@gmail.com>
2023-04-14 02:34:29 -0500

    formatting + remove autocomments

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,7 +6,17 @@ api.nvim_create_augroup("VimRCAutoCmds", { clear = true })
 -- Stop recording of dir history
 vim.g.netrw_dirhistmax = 0
 -- Change .conf syntax highlighting to an aproximate
-api.nvim_create_autocmd(
-  { "BufEnter", "BufRead" },
-  { group = "VimRCAutoCmds", pattern = "*.conf", command = "setf dosini" }
-)
+api.nvim_create_autocmd({ "BufEnter", "BufRead" }, {
+  desc = "Change .conf syntax highlighting to an aproximate",
+  group = "VimRCAutoCmds",
+  pattern = "*.conf",
+  command = "setf dosini",
+})
+
+-- Disable auto-comments!!!
+api.nvim_create_autocmd("FileType", {
+  desc = "Disable auto-comments",
+  group = "VimRCAutoCmds",
+  pattern = "*",
+  command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+})
