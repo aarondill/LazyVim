fdde8480050937f51a75ce5c4c57db4e7aa6a2cd
aarondill <aaronsacks2006@gmail.com>
2023-04-20 23:32:12 -0500

    change augroup to a variable reference vs a string

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,13 +2,13 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 local api = vim.api
-api.nvim_create_augroup("VimRCAutoCmds", { clear = true })
+local VimRCAutoCmds = api.nvim_create_augroup("VimRCAutoCmds", { clear = true })
 -- Stop recording of dir history
 vim.g.netrw_dirhistmax = 0
 -- Change .conf syntax highlighting to an aproximate
 api.nvim_create_autocmd({ "BufEnter", "BufRead" }, {
   desc = "Change .conf syntax highlighting to an aproximate",
-  group = "VimRCAutoCmds",
+  group = VimRCAutoCmds,
   pattern = "*.conf",
   command = "setf dosini",
 })
@@ -16,7 +16,7 @@ api.nvim_create_autocmd({ "BufEnter", "BufRead" }, {
 -- Disable auto-comments!!!
 api.nvim_create_autocmd("FileType", {
   desc = "Disable auto-comments",
-  group = "VimRCAutoCmds",
+  group = VimRCAutoCmds,
   pattern = "*",
   command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
 })
