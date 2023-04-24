d55830b2dfec0fb4c6125b4dca078aba45e0feea
aarondill <aaronsacks2006@gmail.com>
2023-04-24 16:03:21 -0500

    fix lazy loading

--- lua/plugins/smartyank.lua
+++ lua/plugins/smartyank.lua
@@ -1,12 +1,13 @@
 return {
   "ibhagwan/smartyank.nvim",
   opts = { highlight = { timeout = 1000 } },
-  event = "TextYankPost",
+  -- On open file
+  event = { "BufReadPre", "BufNewFile" },
   main = "smartyank",
   config = function(self, opts)
     -- Require main module and setup with opts
     require(self.main).setup(opts)
-    -- Set clipboard to defualt to stop copying to system clipboard on yank
-    vim.g.clipboard = ""
+    -- Set clipboard to nil to stop copying to system clipboard on yank
+    vim.opt.clipboard = nil
   end,
 }
