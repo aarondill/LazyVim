348b4f9afaedf031963ef86077728466e19e952d
aarondill <aaronsacks2006@gmail.com>
2023-04-24 20:04:38 -0500

    move copy_table

--- lua/plugins/overrides/f2-rename.lua
+++ lua/plugins/overrides/f2-rename.lua
@@ -2,12 +2,12 @@ return {
   "neovim/nvim-lspconfig",
   init = function()
     -- Rename with <f2>
-    local util = require("utils")
+    local copy_table = require("utils.copy_table")
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
 
     for _, v in ipairs(keys) do -- Loop each value
       if v.desc:upper() == "RENAME" then -- Search for the rename command
-        local copy = util.copy_table(v) -- Copy so I can modify it
+        local copy = copy_table(v) -- Copy so I can modify it
         copy[1] = "<F2>" -- Change keymap of copy to F2
         keys[#keys + 1] = copy -- Append to keys table
         return -- Stop.
---
+++ lua/utils/copy_table.lua
@@ -0,0 +1,26 @@
+---Local implementation of M.copy_table. Not meant for outside usage.
+---@param obj table a table to copy
+---@param seen table? a table to use for recursive checking
+---@return table copied_table
+local function copy_table(obj, seen)
+  if type(obj) ~= "table" then
+    return obj
+  end
+  if seen and seen[obj] then
+    return seen[obj]
+  end
+  local s = seen or {}
+  local res = setmetatable({}, getmetatable(obj))
+  s[obj] = res
+  for k, v in pairs(obj) do
+    res[copy_table(k, s)] = copy_table(v, s)
+  end
+  return res
+end
+
+---Deep copies a table. Avoid doing this for performance and memory reasons.
+---@param obj table a table to copy
+---@return table copied_table
+return function(obj)
+  return copy_table(obj)
+end
