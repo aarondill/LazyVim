27f44fe2a1dadf404b817ea05f783d95adffb16c
aarondill <aaronsacks2006@gmail.com>
2023-04-24 22:33:08 -0500

    fix dependencies spelling

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,6 @@
 ---@type LazySpec
 return {
   "tpope/vim-fugitive",
-  dependancies = { "aymericbeaumet/vim-symlink", dependancies = { "moll/vim-bbye" } },
+  dependencies = { "aymericbeaumet/vim-symlink", dependancies = { "moll/vim-bbye" } },
   event = "VeryLazy",
 }
