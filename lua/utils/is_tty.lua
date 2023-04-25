9ae434e84262ab5df7af28c79befb24a87ab258a
aarondill <aaronsacks2006@gmail.com>
2023-04-24 21:29:59 -0500

    is_tty desc

--- lua/utils/is_tty.lua
+++ lua/utils/is_tty.lua
@@ -1,3 +1,5 @@
+---returns a boolean indicating whether the current nvim session is running in a tty
+---@return boolean is_tty true if running in a tty, false otherwise
 return function()
   -- false if display is defined, else true
   return not vim.env.DISPLAY
