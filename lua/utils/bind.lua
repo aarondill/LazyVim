376a3f0d71bf11a9c52c13b6c0c5fbe6cfca1514
aarondill <aaronsacks2006@gmail.com>
2023-04-24 21:22:45 -0500

    improve types of utils.bind

--- lua/utils/bind.lua
+++ lua/utils/bind.lua
@@ -1,8 +1,9 @@
 ---returns a curried function that represents f(args, other_args)
+---Note: The types here don't actually work. Don't rely on them for diagnostic purposes.
 ---@generic args, rest, return
----@param f fun(args, rest): return
+---@param f fun(...:args, ...:rest): return
 ---@param ... args
----@return fun(rest): return
+---@return fun(...:rest): return
 return function(f, ...)
   local args = { ... }
   return function(...)
