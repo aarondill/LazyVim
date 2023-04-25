708cda2fab6afc3beca41356a32fe2f1139c2770
aarondill <aaronsacks2006@gmail.com>
2023-04-24 22:36:17 -0500

    remove 'mapping' key

--- lua/plugins/telescope-find-pickers.lua
+++ lua/plugins/telescope-find-pickers.lua
@@ -1,14 +1,13 @@
 -- View telescope pickers and chose one to run (mapped to <leader>t)
 return {
   "keyvchan/telescope-find-pickers.nvim",
-  mapping = "<leader>t",
   config = function()
     require("telescope").load_extension("find_pickers")
   end,
   keys = function(ext)
     return {
       {
-        ext.mapping,
+        "<leader>t",
         require("telescope").extensions.find_pickers.find_pickers,
         desc = "Find Telescope pickers",
         mode = "n",
