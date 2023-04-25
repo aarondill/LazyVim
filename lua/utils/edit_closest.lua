6c132367d06079c19a30121b916ebf60872b3c78
aarondill <aaronsacks2006@gmail.com>
2023-04-24 21:27:22 -0500

    edit_closest returns a bool status

--- lua/utils/edit_closest.lua
+++ lua/utils/edit_closest.lua
@@ -1,14 +1,20 @@
+---Opens the nearest `filename` in the editor, or prints notifies
+---@param filename string the filename to seach for
+---@return boolean success true if file found, false if not found
 return function(filename)
   local match = vim.fs.find(filename, {
     upward = true,
     limit = 1,
     type = "file",
   })
-  if #match > 0 then
-    local file = match[1]
-    vim.notify("Editing " .. vim.fn.fnamemodify(file, ":~:."))
-    vim.cmd.edit(file)
-  else
+
+  if #match == 0 then
     vim.notify("Could not find file: " .. filename)
+    return false
   end
+
+  local file = match[1]
+  vim.notify("Editing " .. vim.fn.fnamemodify(file, ":~:."))
+  vim.cmd.edit(file)
+  return true
 end
