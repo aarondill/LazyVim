62cdbeeb4fcb23305a7ab13cce6d662e98633b17
aarondill <aaronsacks2006@gmail.com>
2023-04-30 09:03:57 -0500

    use vim.tbl_extend

--- lua/plugins/overrides/cmp.lua
+++ lua/plugins/overrides/cmp.lua
@@ -1,10 +1,9 @@
 return {
   "hrsh7th/nvim-cmp",
   opts = function(_, opts)
-    local tbl_extend = require("utils.tbl_extend")
     local cmp = require("cmp")
     -- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
-    return tbl_extend(opts, {
+    return vim.tbl_deep_extend("force", opts or {}, {
       experimental = {
         -- Don't show the ghost text (shown by tabnine)
         ghost_text = false,
