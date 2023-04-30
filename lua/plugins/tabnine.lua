1552d3fd6fcaab275b03adac821fae4c8c0905d5
aarondill <aaronsacks2006@gmail.com>
2023-04-30 09:39:34 -0500

    remove unnecisary config

--- lua/plugins/tabnine.lua
+++ lua/plugins/tabnine.lua
@@ -69,10 +69,6 @@ return {
       local cmp = require("cmp")
       -- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
       return vim.tbl_deep_extend("force", opts or {}, {
-        experimental = {
-          -- Don't show the ghost text (shown by tabnine)
-          ghost_text = false,
-        },
         mapping = {
           -- Accept without explicit selection
           ["<TAB>"] = function(fallback)
