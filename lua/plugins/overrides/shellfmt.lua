569d071d23f8aa0598e68d17e494ff81cdeeaf40
aarondill <aaronsacks2006@gmail.com>
2023-05-02 21:45:15 -0500

    use shiftwidth for shfmt tab width

--- lua/plugins/overrides/shellfmt.lua
+++ lua/plugins/overrides/shellfmt.lua
@@ -4,8 +4,24 @@ return {
     for k, v in ipairs(opts.sources) do
       local nls = require("null-ls")
       if v == nls.builtins.formatting.shfmt then
-        -- Add "-i 2" to arguments for shfmt
-        opts.sources[k] = require("null-ls").builtins.formatting.shfmt.with({ extra_args = { "-i", "2" } })
+        opts.sources[k] = require("null-ls").builtins.formatting.shfmt.with({
+          -- Set shfmt to use the current shift width
+          extra_args = function(params)
+            local lsp_opts = params.options
+            ---@type string
+            local tabSize
+
+            if not lsp_opts then
+              tabSize = "2" -- No options are defined, default to two spaces
+            elseif lsp_opts.insertSpaces == false then
+              tabSize = "0" -- If using tab indentation, use tabs
+            elseif lsp_opts.insertSpaces and lsp_opts.tabSize then
+              tabSize = lsp_opts.tabSize -- Use spaces, according to the current settings
+            end
+
+            return { "-i", tabSize }
+          end,
+        })
       end
     end
   end,
