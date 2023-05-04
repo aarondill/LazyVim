c1f98898b971bdb784f25e74c0038ba873fd31b7
aarondill <aaronsacks2006@gmail.com>
2023-05-04 09:32:00 -0500

    remove dictionary bc it doesn't work

--- lua/plugins/writing.lua
+++ lua/plugins/writing.lua
@@ -1,27 +1,5 @@
 return {
   {
-    "octaltree/cmp-look",
-    lazy = true,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = "octaltree/cmp-look",
-  },
-  opts = function(_, opts)
-    local cmp = require("cmp")
-    opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
-      {
-        name = "look",
-        keyword_length = 2,
-        option = {
-          convert_case = true,
-          loud = true,
-          dict = "/usr/share/dict/words",
-        },
-      },
-    }))
-  end,
-  {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "proselint")
