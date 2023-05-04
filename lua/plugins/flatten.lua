f01678bb4de44d58dffb88ee4f99dd91da42c41e
aarondill <aaronsacks2006@gmail.com>
2023-05-04 16:26:59 -0500

    document flatten.nvim

--- lua/plugins/flatten.lua
+++ lua/plugins/flatten.lua
@@ -1,3 +1,4 @@
+---@type LazySpec
 return {
   "willothy/flatten.nvim",
   opts = {
@@ -9,8 +10,23 @@ return {
     allow_cmd_passthrough = true,
     -- Allow a nested session to open if Neovim is opened without arguments
     nest_if_no_args = false,
+    -- Window options
+    window = {
+      -- Options:
+      -- current        -> open in current window (default)
+      -- alternate      -> open in alternate window (recommended)
+      -- tab            -> open in new tab
+      -- split          -> open in split
+      -- vsplit         -> open in vsplit
+      open = "alternate",
+      -- Affects which file gets focused when opening multiple at once
+      -- Options:
+      -- "first"        -> open first file of new files (default)
+      -- "last"         -> open last file of new files
+      focus = "first",
+    },
   },
   -- Ensure that it runs first to minimize delay when opening file from terminal
   lazy = false,
-  priority = 1001,
+  priority = 100001,
 }
