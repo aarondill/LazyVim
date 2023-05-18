ad386c7201d1d4efc2481bdef5e833a3fd771d18
aarondill <aaronsacks2006@gmail.com>
2023-05-18 12:50:13 -0500

    remove proselint

--- lua/plugins/writing.lua
+++ lua/plugins/writing.lua
@@ -2,7 +2,6 @@ return {
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      table.insert(opts.ensure_installed, "proselint")
       table.insert(opts.ensure_installed, "write-good")
       table.insert(opts.ensure_installed, "alex")
     end,
@@ -12,8 +11,6 @@ return {
     opts = function(_, opts)
       local nls = require("null-ls")
       opts.sources = vim.list_extend(opts.sources, {
-        nls.builtins.diagnostics.proselint.with({ extra_filetypes = { "markdown", "text" } }),
-        nls.builtins.code_actions.proselint.with({ extra_filetypes = { "markdown", "text" } }),
         nls.builtins.diagnostics.alex.with({ extra_filetypes = { "markdown", "text" } }),
         nls.builtins.diagnostics.write_good.with({ extra_filetypes = { "markdown", "text" } }),
       })
