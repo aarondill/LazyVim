cf4a8037fcf6d6441b293e550ca4b121a657acac
aarondill <aaronsacks2006@gmail.com>
2023-05-25 07:16:34 -0500

    re-add suda_smart_edit?

--- lua/plugins/suda.lua
+++ lua/plugins/suda.lua
@@ -2,9 +2,7 @@ return {
   "lambdalisue/suda.vim",
   cond = vim.fn.executable("sudo"),
   init = function()
-    -- Removed because it doesn't work with readable files sometimes
-    -- Run SudaWrite instead
-    -- vim.g.suda_smart_edit = 1
+    vim.g.suda_smart_edit = 1
   end,
   cmd = { "SudaRead", "SudaWrite" },
   -- Load on enter *any* file, required for smart editing.
