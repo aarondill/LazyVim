49117f8e0d5cf122ed363ca5e9c2e2491b966669
aarondill <aaronsacks2006@gmail.com>
2023-06-18 16:01:28 -0500

    math.huge instead of 1001

--- lua/plugins/chezmoi.lua
+++ lua/plugins/chezmoi.lua
@@ -3,7 +3,7 @@ return {
   "alker0/chezmoi.vim",
   lazy = false,
   -- Really big number ¯\_(ツ)_/¯ (default: 50)
-  priority = 1001,
+  priority = math.huge,
   init = function()
     vim.g["chezmoi#use_external"] = 1
   end,
