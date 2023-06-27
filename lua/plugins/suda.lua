34f4278f75b181cadfaae78909a086cff995e04c
aarondill <aaronsacks2006@gmail.com>
2023-06-27 00:09:05 -0500

    disable suda_smart_edit because it breaks lsp

--- lua/plugins/suda.lua
+++ lua/plugins/suda.lua
@@ -1,14 +1,14 @@
 return {
   "lambdalisue/suda.vim",
   cond = vim.fn.executable("sudo"),
-  init = function()
-    vim.g.suda_smart_edit = 1
-  end,
   cmd = { "SudaRead", "SudaWrite" },
-  -- Load on enter *any* file, required for smart editing.
-  event = {
-    "BufReadPre",
-    "BufNewFile",
-    -- "BufEnter"
-  },
+  -- init = function()
+  --   vim.g.suda_smart_edit = 1
+  -- end,
+  -- -- Load on enter *any* file, required for smart editing.
+  -- event = {
+  --   "BufReadPre",
+  --   "BufNewFile",
+  --   -- "BufEnter"
+  -- },
 }
