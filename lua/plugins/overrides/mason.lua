75e4725af0f3f3e2ab56467f372276f5267693a0
aarondill <aaronsacks2006@gmail.com>
2023-07-26 22:46:29 -0500

    mason ensure installed

---
+++ lua/plugins/overrides/mason.lua
@@ -0,0 +1,28 @@
+local ensure_installed_table = {
+  "lua-language-server", -- lua
+  "bash-language-server", -- bash
+  "clangd", -- cpp
+  "cspell", -- spelling
+  "eslint-lsp", -- eslint
+  "eslint_d", -- eslint
+  "gopls", -- go
+  "jdtls", -- java
+  "json-lsp", -- json
+  "prettier", --prettier
+  "shellcheck", --shell
+  "taplo", -- toml
+  "typescript-language-server", -- Typescript
+  "vim-language-server", -- vimscript
+  "vint", -- vimscript
+}
+
+return {
+  "williamboman/mason.nvim",
+  opts = function(_, opts)
+    opts = opts or {}
+    opts.ensure_installed = opts.ensure_installed or {}
+    if type(opts.ensure_installed) == "table" then
+      vim.list_extend(opts.ensure_installed, ensure_installed_table)
+    end
+  end,
+}
