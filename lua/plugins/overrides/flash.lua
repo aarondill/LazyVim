28f37fef88155ac3013d12b4523553ca6222534c
aarondill <aaronsacks2006@gmail.com>
2023-07-28 01:39:10 -0500

    fully disable flash in searching
    I hate this when there's no match for what I'm looking for.

--- lua/plugins/overrides/flash.lua
+++ lua/plugins/overrides/flash.lua
@@ -3,10 +3,7 @@ return {
   ---@type Flash.Config
   opts = {
     modes = {
-      search = {
-        -- Don't jump in search. This helps avoid jumping when there's no match for your search string
-        jump = { history = false, register = false, nohlsearch = false },
-      },
+      search = { enabled = false },
     },
   },
 }
