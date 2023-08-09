ae3a44a9f9dffa1d9650da29816d12c589c3ef74
aarondill <aaronsacks2006@gmail.com>
2023-08-09 08:25:19 -0500

    move simple icon overrides to icons.lua

--- lua/plugins/overrides/icons.lua
+++ lua/plugins/overrides/icons.lua
@@ -9,4 +9,26 @@ return {
       },
     },
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = icons.gitsigns.add },
+        change = { text = icons.gitsigns.change },
+        delete = { text = icons.gitsigns.delete },
+        topdelete = { text = icons.gitsigns.topdelete },
+        changedelete = { text = icons.gitsigns.changedelete },
+        untracked = { text = icons.gitsigns.untracked },
+      },
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      opts = {
+        defaults = {
+          prompt_prefix = icons["telescope"].prompt_prefix,
+          selection_caret = icons["telescope"].selection_caret,
+        },
+      },
+    },
+  },
 }
