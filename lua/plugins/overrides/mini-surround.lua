8fd036a827336f61216b1600851aaf954d6eafcd
aarondill <aaronsacks2006@gmail.com>
2023-08-14 04:20:16 -0500

    respect selection type mini-surround

--- lua/plugins/overrides/mini-surround.lua
+++ lua/plugins/overrides/mini-surround.lua
@@ -1,5 +1,14 @@
 return {
   "echasnovski/mini.surround",
+  opts = {
+    -- Number of lines within which surrounding is searched
+    n_lines = 50,
+
+    -- Whether to respect selection type:
+    -- - Place surroundings on separate lines in linewise mode.
+    -- - Place surroundings on each line in blockwise mode.
+    respect_selection_type = true,
+  },
   keys = function(_, keys)
     local chars = {
       ["("] = ")",
