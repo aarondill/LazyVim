ca1697bae3c5be214569f26a1133b08629543e0b
aarondill <aaronsacks2006@gmail.com>
2023-08-17 19:16:03 -0500

    simplify cmp snippets_down handler

--- lua/plugins/overrides/cmp.lua
+++ lua/plugins/overrides/cmp.lua
@@ -14,17 +14,9 @@ end
 local function snippets_down(entry1, entry2)
   local types = require("cmp.types")
   local kind1, kind2 = entry1:get_kind(), entry2:get_kind()
-  kind1 = kind1 == types.lsp.CompletionItemKind.Text and 100 or kind1
-  kind2 = kind2 == types.lsp.CompletionItemKind.Text and 100 or kind2
   if kind1 == kind2 then return nil end -- no preference if same
   if kind1 == types.lsp.CompletionItemKind.Snippet then return false end -- Discourage snippets
   if kind2 == types.lsp.CompletionItemKind.Snippet then return true end -- Discourage snippets
-  local diff = kind1 - kind2
-  if diff < 0 then
-    return true
-  elseif diff > 0 then
-    return false
-  end
 end
 
 return {
@@ -56,7 +48,7 @@ return {
           cmp.config.compare.recently_used,
           under_cmp,
           snippets_down,
-          -- cmp.config.compare.kind, -- replaced by lsp_first
+          cmp.config.compare.kind, -- superseded by lsp_first -- just used for lowering text
           cmp.config.compare.sort_text,
           cmp.config.compare.length,
           cmp.config.compare.order,
