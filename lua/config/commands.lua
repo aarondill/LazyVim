597d30740012194feb0443026abb9b2e69144e2b
aarondill <aaronsacks2006@gmail.com>
2023-10-02 23:43:10 -0500

    add RandomLine command

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,3 +1,7 @@
 -- Setup user commands here. This is laoded by init.lua
 
 vim.api.nvim_create_user_command("DiffSaved", require("utils.diff_with_saved"), {})
+vim.api.nvim_create_user_command("RandomLine", function()
+  local l = math.random(1, vim.fn.line("$") or 1) -- Get random number upto last line
+  vim.cmd("normal! " .. l .. "G")
+end, {})
