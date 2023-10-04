a04dade426db9af20b92451b19fb307b4b5b2079
aarondill <aaronsacks2006@gmail.com>
2023-10-04 15:15:51 -0500

    add UniqLines command

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -5,3 +5,7 @@ vim.api.nvim_create_user_command("RandomLine", function()
   local l = math.random(1, vim.fn.line("$") or 1) -- Get random number upto last line
   vim.cmd("normal! " .. l .. "G")
 end, {})
+vim.api.nvim_create_user_command("UniqLines", function()
+  pcall(vim.cmd, [[%s/^\(.*\)\(\n\1\)\+$/\1/]])
+  pcall(vim.cmd, [[%s/\v^(.*)(\n\1)+$/\1/]])
+end, {})
