b351675d3082e58d43b37fafb9b749e3c7fe5ecc
aarondill <aaronsacks2006@gmail.com>
2023-10-12 14:17:38 -0500

    revert tbl_extend changes

--- lua/utils/tbl_extend.lua
+++ lua/utils/tbl_extend.lua
@@ -1,24 +1,7 @@
-local M = {}
 ---merges table b into table a
 ---@param a table? the table to merge into.
 ---@param b table? the table to merge properties from.
 ---@return table merged a *new* table with merged properties from both tables.
-function M.deep_extend(a, b)
+return function(a, b)
   return vim.tbl_deep_extend("force", a or {}, b or {}) or {}
 end
----@param ... unknown[]
----@return unknown[]
-function M.join(...)
-  local ret = {}
-  for _, t in ipairs({ ... }) do -- Each table provided
-    for _, v in ipairs(t) do -- Each key of the tables
-      table.insert(ret, v) -- Append to ret.
-    end
-  end
-  return ret
-end
-return setmetatable(M, {
-  __call = function(_, ...)
-    return M.deep_extend(...) -- Backwards compatibility
-  end,
-})
