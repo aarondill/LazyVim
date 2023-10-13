8de25e56a5d781fe6989b163a064de01162e4648
aarondill <aaronsacks2006@gmail.com>
2023-10-13 04:00:09 -0500

    don't require telescope until needed

--- lua/plugins/telescope-find-pickers.lua
+++ lua/plugins/telescope-find-pickers.lua
@@ -1,4 +1,5 @@
 -- View telescope pickers and chose one to run (mapped to <leader>t)
+---@type LazySpec
 return {
   "keyvchan/telescope-find-pickers.nvim",
   dependencies = "nvim-telescope/telescope.nvim",
@@ -6,16 +7,16 @@ return {
     local ok, mod = pcall(require, "telescope")
     if ok then mod.load_extension("find_pickers") end
   end,
-  keys = function(ext)
-    local ok, tlsp = pcall(require, "telescope")
-    if not ok then return {} end
-    return {
-      {
-        "<leader>t",
-        tlsp.extensions.find_pickers.find_pickers,
-        desc = "Find Telescope pickers",
-        mode = "n",
-      },
-    }
-  end,
+  keys = {
+    {
+      "<leader>t",
+      function()
+        local ok, tlsp = pcall(require, "telescope")
+        assert(ok, "telescope-find-pickers requires telescope! Please ensure it's installed")
+        return tlsp.extensions.find_pickers.find_pickers()
+      end,
+      desc = "Find Telescope pickers",
+      mode = "n",
+    },
+  },
 }
