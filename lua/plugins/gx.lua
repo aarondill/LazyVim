2a6150907a01f3012aa32a2503c1c134fe268da6
aarondill <aaronsacks2006@gmail.com>
2023-10-13 05:02:39 -0500

    handle gf keymap better

--- lua/plugins/gx.lua
+++ lua/plugins/gx.lua
@@ -1,18 +1,32 @@
-local set_key_map = require("utils.set_key_map")
-local vtext = require("utils.vtext")
+---@param err string
+---@return string
+local function get_err_str(err)
+  local _, m = err:match("(%S*%(%):%s*Vim%(%S*%):%s*)(.*)")
+  return m or err
+end
+---@type LazySpec
 return {
   "chrishrb/gx.nvim",
-  event = { "BufEnter" },
+  event = { "LazyFile" },
   dependencies = { "nvim-lua/plenary.nvim" },
-  init = function()
-    set_key_map({ "n", "x" }, "gf", function()
-      local cursor_file = vtext() or vim.fn.expand("<cfile>")
-      if type(cursor_file) == "string" and cursor_file:match("^https?://.*$") then
-        vim.cmd("norm gx")
-      else
-        vim.cmd("norm! gf")
-      end
-    end)
-  end,
+  keys = {
+    { "gx" },
+    {
+      "gf",
+      function()
+        local vtext = require("utils.vtext")
+        local cursor_file = vtext() or vim.fn.expand("<cfile>")
+        -- Found a url. Use gx to open it!
+        if type(cursor_file) == "string" and cursor_file:match("^https?://.*$") then return vim.cmd("norm gx") end
+
+        ---Emulate vim's nicer error notifs:
+        ---@diagnostic disable-next-line :param-type-mismatch vim.cmd can be called. let me.
+        local ok, err = pcall(vim.cmd, "norm! gf")
+        if ok then return end
+        return vim.notify(get_err_str(err), vim.log.levels.ERROR) -- notify on error
+      end,
+      mode = { "v", "n" },
+    },
+  },
   config = true,
 }
