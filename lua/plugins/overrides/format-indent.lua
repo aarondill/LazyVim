a76e7398748f768bebbae7141c2c7276a5ad1bf5
aarondill <aaronsacks2006@gmail.com>
2023-10-16 01:36:30 -0500

    We can *greatly* simplify format-indent now :)
    Thank god for https://github.com/stevearc/conform.nvim/pull/140!
    
    with prepend_args, I no longer have to attempt to parse the default arguments!

--- lua/plugins/overrides/format-indent.lua
+++ lua/plugins/overrides/format-indent.lua
@@ -1,57 +1,29 @@
-local bufutils = require("utils.buf")
-
----@generic Ctx :conform.Context|conform.RangeContext
----@param ctx Ctx
----@param args string|string[]|fun(ctx: Ctx):(string[]|string)
----@return string[]|string
-local function extract(args, ctx)
-  if type(args) == "function" then return args(ctx) or {} end
-  return args or {}
-end
-
 return {
   "stevearc/conform.nvim",
   opts = {
     formatters = {
       -- Set stylua to use the current shift width
-      ---@type conform.FormatterConfig
+      ---@type conform.FormatterConfigOverride
       stylua = {
-        command = "stylua", -- Required for type checking.
-        args = function(ctx)
-          local args = extract(require("conform.formatters.stylua").args, ctx) ---@cast args string[]|{n: integer?}
-          if type(args) == "string" then return args end -- I give up.
+        prepend_args = function(ctx)
+          local bufutils = require("utils.buf")
           local i = bufutils.get_indent(ctx.buf)
           return {
             "--indent-width",
-            tostring(i.size),
+            i.size,
             "--indent-type",
             i.tabs and "Tabs" or "Spaces",
-            unpack(args, 1, args.n),
-          } -- Unpack must be last.
-        end,
-        range_args = function(ctx)
-          local args = extract(require("conform.formatters.stylua").range_args, ctx)
-          if type(args) == "string" then return args end -- I give up.
-          local i = bufutils.get_indent(ctx.buf) ---@cast args string[]|{n: integer?}
-          return {
-            "--indent-width",
-            tostring(i.size),
-            "--indent-type",
-            i.tabs and "Tabs" or "Spaces",
-            unpack(args, 1, args.n),
           }
         end,
       },
 
       -- Set shfmt to use the current shift width
-      ---@type conform.FormatterConfig
+      ---@type conform.FormatterConfigOverride
       shfmt = {
-        command = "shfmt", -- Required for type checking.
-        args = function(ctx)
-          local args = extract(require("conform.formatters.shfmt").args, ctx)
-          if type(args) == "string" then return args end -- I give up.
-          local i = bufutils.get_indent(ctx.buf) ---@cast args string[]|{n: integer?}
-          return { "-i", i.tabs and 0 or i.size, unpack(args, 1, args.n) }
+        prepend_args = function(ctx)
+          local bufutils = require("utils.buf")
+          local i = bufutils.get_indent(ctx.buf)
+          return { "-i", i.tabs and 0 or i.size }
         end,
       },
     },
