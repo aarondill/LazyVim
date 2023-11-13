c9d5ae6138dc473794cd8126292366ebc4c4fe29
aarondill <aaronsacks2006@gmail.com>
2023-11-13 13:16:40 -0600

    setup smartyank clipboard on init

--- lua/plugins/smartyank.lua
+++ lua/plugins/smartyank.lua
@@ -4,10 +4,9 @@ return {
   -- On open file
   event = { "LazyFile" },
   main = "smartyank",
-  config = function(self, opts)
-    -- Require main module and setup with opts
-    require(self.main).setup(opts)
+  init = function()
     -- Set clipboard to nil to stop copying to system clipboard on yank
     vim.opt.clipboard = nil
   end,
+  config = true,
 }
