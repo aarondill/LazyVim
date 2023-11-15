cceca97dcdb5576a1c8b8e91d9e531ae5c09fb59
aarondill <aaronsacks2006@gmail.com>
2023-11-15 13:58:13 -0600

    fix: java has a lot of problems
    I don't know all of them, but i think this fixes many

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,3 +1,36 @@
+local function setup_jdtls(opts)
+  local jdtls = require("jdtls")
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  local has_wk, wk = pcall(require, "which-key")
+  if has_wk then wk.register({
+    ["<leader>cJ"] = { name = "+java", buffer = bufnr, mode = { "n", "v" } },
+  }) end
+
+  local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle", "package.json" }
+  local root_dir = require("jdtls.setup").find_root(root_markers)
+
+  -- If you started neovim within `~/dev/xy/project-1` this would resolve to `project-1`
+  local project_name = vim.fn.fnamemodify(root_dir or vim.fn.getcwd(), ":p:h:t")
+  local cachedir = vim.fn.stdpath("cache")
+  assert(type(cachedir) == "string")
+  local workspace_dir = vim.fs.joinpath(cachedir, "jdtls", "workspace", project_name)
+
+  local config = vim.tbl_extend("force", opts, { root_dir = root_dir })
+  vim.list_extend(config.cmd, { "-data", workspace_dir })
+  jdtls.start_or_attach(config)
+  -- DAP is available? set it up.
+  if pcall(require, "dap") then jdtls.setup_dap({ hotcodereplace = "auto", config_overrides = {} }) end
+end
+
+---@param method string
+local function lazy_method(method, ...)
+  local args = select("#", ...) > 0 and table.pack(...) or nil
+  return function()
+    if not args then return require("jdtls")[method]() end
+    return require("jdtls")[method](table.unpack(args, 1, args.n))
+  end
+end
 return {
   {
     "neovim/nvim-lspconfig",
@@ -8,6 +41,21 @@ return {
       ---@type lspconfig.options | {}
       servers = {
         jdtls = {
+          ---@type LazyKeysSpec[]
+          keys = {
+            { "<leader>cJi", lazy_method("organize_imports"), desc = "Organize imports" },
+            { "<leader>cJt", lazy_method("test_class"), desc = "Test class" },
+            { "<leader>cJn", lazy_method("test_nearest_method"), desc = "Test nearest method" },
+            { "<leader>cJe", lazy_method("extract_variable"), desc = "Extract variable" },
+            { "<leader>cJM", lazy_method("extract_method"), desc = "Extract method" },
+            {
+              "<leader>cJe",
+              lazy_method("extract_variable", { visual = true }),
+              desc = "Extract variable",
+              mode = "v",
+            },
+            { "<leader>cJM", lazy_method("extract_method", { visual = true }), desc = "Extract method", mode = "v" },
+          },
           cmd = { "jdtls" },
           settings = {
             redhat = {
@@ -34,59 +82,9 @@ return {
           vim.api.nvim_create_autocmd("FileType", {
             pattern = "java",
             callback = function()
-              local jdtls = require("jdtls")
-              local wk = require("which-key")
-              local bufnr = vim.api.nvim_get_current_buf()
-
-              local v_extract_variable = function()
-                jdtls.extract_variable({ visual = true })
-              end
-              local v_extract_method = function()
-                jdtls.extract_method({ visual = true })
-              end
-
-              wk.register({
-                ["<leader>cJ"] = { name = "+java", buffer = bufnr, mode = { "n", "v" } },
-              })
-
-              wk.register({
-                i = { jdtls.organize_importsorganize_imports, "Organize imports" },
-                t = { jdtls.test_class, "Test class" },
-                n = { jdtls.test_nearest_method, "Test nearest method" },
-                e = { jdtls.extract_variable, "Extract variable" },
-                M = { jdtls.extract_method, "Extract method" },
-              }, {
-                prefix = "<leader>cJ",
-                buffer = bufnr,
-              })
-
-              wk.register({
-                e = { v_extract_variable, "Extract variable" },
-                M = { v_extract_method, "Extract method" },
-              }, {
-                mode = "v",
-                prefix = "<leader>cJ",
-                buffer = bufnr,
-              })
-
-              -- If you started neovim within `~/dev/xy/project-1` this would resolve to `project-1`
-              local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-              local XDG_DATA_HOME = os.getenv("XDG_DATA_HOME") or (os.getenv("HOME") .. "/.local/share")
-              local workspace_dir = vim.fn.simplify(("%s/%s/%s"):format(XDG_DATA_HOME, "/jdtls/", project_name))
-
-              local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle", "package.json" }
-              local root_dir = require("jdtls.setup").find_root(root_markers)
-              local config = vim.tbl_extend(
-                "force",
-                opts,
-                { root_dir = root_dir, cmd = vim.tbl_flatten({ opts.cmd, { "-data", workspace_dir } }) }
-              )
-              jdtls.start_or_attach(config)
-              -- DAP is available? set it up.
-              if pcall(require, "dap") then jdtls.setup_dap({ hotcodereplace = "auto", config_overrides = {} }) end
+              return setup_jdtls(opts)
             end,
           })
-
           return true
         end,
       },
