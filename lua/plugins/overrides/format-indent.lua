a6abaa148382e4e4ce7a8142a2de9c3732d05393
aarondill <aaronsacks2006@gmail.com>
2023-12-08 09:30:11 -0600

    fix: compat requires first argument be named 'self' exactly.

--- lua/plugins/overrides/format-indent.lua
+++ lua/plugins/overrides/format-indent.lua
@@ -5,7 +5,11 @@ return {
       -- Set stylua to use the current shift width
       ---@type conform.FormatterConfigOverride
       stylua = {
-        prepend_args = function(_self, ctx)
+        prepend_args = function(self, ctx)
+          --- Note: this is useless, but avoids a warning about unused parameters
+          --- conform currently has a compatability layer, which forces the first
+          --- argument to be called 'self', even if not being used.
+          self = self
           local bufutils = require("utils.buf")
           local i = bufutils.get_indent(ctx.buf)
           return {
@@ -20,7 +24,11 @@ return {
       -- Set shfmt to use the current shift width
       ---@type conform.FormatterConfigOverride
       shfmt = {
-        prepend_args = function(_self, ctx)
+        prepend_args = function(self, ctx)
+          --- Note: this is useless, but avoids a warning about unused parameters
+          --- conform currently has a compatability layer, which forces the first
+          --- argument to be called 'self', even if not being used.
+          self = self
           local bufutils = require("utils.buf")
           local i = bufutils.get_indent(ctx.buf)
           return { "-i", i.tabs and 0 or i.size }
