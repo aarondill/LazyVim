4692d92bb2d5ef2411706e6b6e791a1ed13ca576
aarondill <aaronsacks2006@gmail.com>
2024-02-02 01:46:39 -0600

    refactor: remove redundant code in neo-tree override
    use vim.tbl_extend_deep instead of specifying each key

--- lua/plugins/overrides/neo-tree.lua
+++ lua/plugins/overrides/neo-tree.lua
@@ -1,41 +1,11 @@
 local icons = require("config.icons")
+local neoicons = icons["neo-tree"] or {}
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    default_component_configs = {
-      indent = {
-        expander_collapsed = icons["neo-tree"].default_component_configs.indent.expander_collapsed,
-        expander_expanded = icons["neo-tree"].default_component_configs.indent.expander_expanded,
-        indent_marker = icons["neo-tree"].default_component_configs.indent.indent_marker,
-        last_indent_marker = icons["neo-tree"].default_component_configs.indent.last_indent_marker,
-      },
-      icon = {
-        folder_closed = icons["neo-tree"].default_component_configs.icon.folder_closed,
-        folder_open = icons["neo-tree"].default_component_configs.icon.folder_open,
-        folder_empty = icons["neo-tree"].default_component_configs.icon.folder_empty,
-        folder_empty_open = icons["neo-tree"].default_component_configs.icon.folder_empty_open,
-        default = icons["neo-tree"].default_component_configs.icon.default,
-      },
-      git_status = {
-        symbols = {
-          -- Change type
-          added = icons["neo-tree"].default_component_configs.git_status.symbols.added, -- or "✚", but this is redundant info if you use git_status_colors on the name
-          modified = icons["neo-tree"].default_component_configs.git_status.symbols.modified, -- or "", but this is redundant info if you use git_status_colors on the name
-          deleted = icons["neo-tree"].default_component_configs.git_status.symbols.deleted, -- this can only be used in the git_status source
-          renamed = icons["neo-tree"].default_component_configs.git_status.symbols.renamed, -- this can only be used in the git_status source
-          -- Status type
-          untracked = icons["neo-tree"].default_component_configs.git_status.symbols.untracked,
-          ignored = icons["neo-tree"].default_component_configs.git_status.symbols.ignored,
-          unstaged = icons["neo-tree"].default_component_configs.git_status.symbols.unstaged,
-          staged = icons["neo-tree"].default_component_configs.git_status.symbols.staged,
-          conflict = icons["neo-tree"].default_component_configs.git_status.symbols.conflict,
-        },
-      },
-      modified = { symbol = icons["neo-tree"].default_component_configs.modified.symbol },
-    },
+  opts = vim.tbl_deep_extend("force", neoicons, {
     window = {
       mappings = {
-        ["o"] = "open",
+        ["o"] = "open", -- Open on 'o'
       },
     },
     filesystem = {
@@ -46,5 +16,5 @@ return {
         hide_hidden = false,
       },
     },
-  },
+  }),
 }
