2b6b87e12e2c106d3d5d672726848d0750cf7565
aarondill <aaronsacks2006@gmail.com>
2024-02-06 10:24:56 -0600

    fix: always require autocmds module before verylazy
    this ensures that VimEnter or VeryLazy autocmds can still run.
    
    These *won't* be run twice because requiring the same file twice doesn't do anything.

--- init.lua
+++ init.lua
@@ -8,21 +8,12 @@ vim.uv = vim.uv or vim.loop
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
----This should *not* load keymaps, options, lazy, or autocmds
-local function load_config()
-  require("config.commands")
-  -- If running in tty, set menu transparency to opaque
-  if require("utils.is_tty")() then require("config.tty") end
-end
-local has_lazyvim_util, lazyvim_util = pcall(require, "lazyvim.util")
-if has_lazyvim_util then
-  lazyvim_util.on_very_lazy(load_config)
-else
-  load_config()
-  require("config.keymaps")
-  require("config.options")
-  require("config.autocmds")
-end
+-- If running in tty, set menu transparency to opaque
+if require("utils.is_tty")() then require("config.tty") end
+require("config.commands")
+require("config.keymaps")
+require("config.options")
+require("config.autocmds")
 
 --- Handle regenerating helptags in new VIMRUNTIMEs
 local rt = os.getenv("VIMRUNTIME")
