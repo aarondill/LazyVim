dc81c35b52c4937f83236559c4cd0562964e2ee5
aarondill <aaronsacks2006@gmail.com>
2024-02-25 03:11:26 -0600

    refactor: merge tokyonight config into colorscheme
    note: also uninstalls cattpuccin

--- lua/plugins/overrides/colorscheme.lua
+++ lua/plugins/overrides/colorscheme.lua
@@ -2,19 +2,43 @@
 ---@type LazySpec
 return {
   {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      transparent = true, -- Enable this to disable setting the background color
+      dim_inactive = true, -- dims inactive windows
+      style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    enabled = false, -- don't even install catppuccin
+  },
+  -- Set to transparent (to avoid error notification)
+  { "rcarriga/nvim-notify", optional = true, opts = { background_colour = "#000000" } },
+  {
     "LazyVim/LazyVim",
     opts = function(_, opts)
       local is_tty = require("utils.is_tty")
-      local tbl_extend = require("utils.tbl_extend")
-      local loc_opts = {}
-      loc_opts.colorscheme = function()
-        if is_tty() then
-          if not pcall(vim.cmd.colorscheme, "wildcharm") then pcall(vim.cmd.colorscheme, "pablo") end
-        else
-          require("tokyonight").load()
+      if is_tty() then
+        opts.colorscheme = function()
+          local themes = { "wildcharm", "pablo" }
+          for _, t in ipairs(themes) do
+            local ok = pcall(vim.cmd.colorscheme, t)
+            if ok then break end
+          end
+        end
+      else
+        opts.colorscheme = function()
+          return require("tokyonight").load()
         end
       end
-      return tbl_extend(opts, loc_opts)
     end,
   },
 }
