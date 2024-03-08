8b17a85bb6d60b01912c99e4e317a18d3c431a5e
aarondill <aaronsacks2006@gmail.com>
2024-03-08 00:47:46 -0600

    fix: typo, vim.fs should be vim.fs.joinpath

--- lua/future.lua
+++ lua/future.lua
@@ -1,7 +1,8 @@
 -- Fowards compatability:
 table.pack = table.pack or function(...) return { n = select("#", ...), ... } end
 table.unpack = table.unpack or unpack
-vim.uv = vim.uv or vim.loop
+local uv = vim.uv or vim.loop
+vim.uv, vim.loop = uv, uv
 ---@param ... string
 ---@return string
-vim.fs = vim.fs or function(...) return (table.concat({ ... }, "/"):gsub("//+", "/")) end
+vim.fs.joinpath = vim.fs.joinpath or function(...) return (table.concat({ ... }, "/"):gsub("//+", "/")) end
