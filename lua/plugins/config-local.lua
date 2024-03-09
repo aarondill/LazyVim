cf72ae9bf6be6f83620eea487f946f6aed26fee2
aarondill <aaronsacks2006@gmail.com>
2024-03-09 01:11:33 -0600

    fix: load config-local on startup
    This allows things to be set like filetype associations

--- lua/plugins/config-local.lua
+++ lua/plugins/config-local.lua
@@ -1,9 +1,17 @@
+---Note: This plugin can't be lazy loaded because the config can change values that need to be set early on.
+---If you wish to lazy load something in the local config, then set it!
+
 ---@type LazySpec
 return {
   "klen/nvim-config-local",
   main = "config-local",
-  cmd = { "ConfigLocalEdit", "ConfigLocalSource", "ConfigLocalTrust", "ConfigLocalIgnore" },
-  event = { "VimEnter", "DirChanged", "BufWritePost", "BufRead" },
+  init = function(self)
+    local config_local = require(self.main) ---@module "config-local"
+    config_local.setup(self.opts)
+    config_local.source() -- force load on startup
+  end,
+  config = function() end,
+  lazy = false,
   opts = {
     -- Config file patterns to load (lua supported)
     config_files = { ".nvim.lua", ".nvimrc", ".exrc", ".nvim/init.lua" },
