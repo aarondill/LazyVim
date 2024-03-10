c17203f76d7b2ba77e4ef4e8e967be11a0ea2203
aarondill <aaronsacks2006@gmail.com>
2024-03-09 23:14:25 -0600

    fix: hack together a way to dismiss the noice.nvim search_count

--- lua/plugins/highlight-current-n.lua
+++ lua/plugins/highlight-current-n.lua
@@ -1,3 +1,35 @@
+local hcn
+do
+  --- HACK: Clear the noice search_count when moving
+  local jumping = false -- keep track of if we're jumping to the next match
+  local autocmd = nil -- the cached autocmd id. Used to make sure that CursorMoved isn't *always* running
+  local function clear_noice()
+    if autocmd then return end -- cache it, we only need one autocmd
+    if not package.loaded["noice"] then return end -- noice hasn't loaded, we can skip this
+    autocmd = vim.api.nvim_create_autocmd({ "CursorMoved", "InsertEnter", "CmdlineEnter" }, {
+      callback = function()
+        if jumping then return end -- this was a jump, keep the count
+        local m = require("noice.ui.msg").get("msg_show", "search_count")
+        require("noice.message.manager").remove(m)
+        autocmd = nil
+        return true -- remove the autocmd
+      end,
+    })
+  end
+
+  ---Calls key on highlight_current_n and handles jumping
+  ---@param key 'n'|'N'
+  ---@return fun()
+  function hcn(key)
+    return function()
+      jumping = true
+      require("highlight_current_n")[key]()
+      vim.schedule(function() jumping = false end)
+      clear_noice()
+    end
+  end
+end
+
 return {
   "rktjmp/highlight-current-n.nvim",
   opts = { highlight_group = "IncSearch" },
@@ -7,14 +39,12 @@ return {
     local ClearSearchHL = vim.api.nvim_create_augroup("ClearSearchHL", { clear = true })
     -- You may only want to see hlsearch /while/ searching, you can automatically
     -- toggle hlsearch with the following autocommands
-    vim.api.nvim_create_autocmd("CmdlineEnter", {
+    vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
       pattern = { "/", "\\?" },
-      callback = function() vim.opt.hlsearch = true end,
-      group = ClearSearchHL,
-    })
-    vim.api.nvim_create_autocmd("CmdlineLeave", {
-      pattern = { "/", "\\?" },
-      callback = function() vim.opt.hlsearch = false end,
+      callback = function(ev)
+        local val = ev.event == "CmdlineEnter" -- enable on enter, disable on exit
+        vim.opt.hlsearch = val
+      end,
       group = ClearSearchHL,
     })
     -- this will apply similar n|N highlighting to the first search result
@@ -24,8 +54,5 @@ return {
       group = ClearSearchHL,
     })
   end,
-  keys = {
-    { "n", function() require("highlight_current_n").n() end, mode = "n" },
-    { "N", function() require("highlight_current_n").N() end, mode = "n" },
-  },
+  keys = { { "n", hcn("n"), mode = "n" }, { "N", hcn("N"), mode = "n" } },
 }
