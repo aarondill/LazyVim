33fad9e5757f35e6ede0acbe2f0cfbe55a6b4997
aarondill <aaronsacks2006@gmail.com>
2024-03-09 21:27:49 -0600

    refactor: no function to call in add-borders.lua

--- lua/plugins/overrides/add-borders.lua
+++ lua/plugins/overrides/add-borders.lua
@@ -1,22 +1,16 @@
 return {
   {
-    "folke/noice.nvim", -- Use noice's menu for inc_rename bc nicer than dressing's
+    "folke/noice.nvim",
     opts = { presets = { lsp_doc_border = true } },
   },
   {
     "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      -- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
-      return vim.tbl_deep_extend("force", opts or {}, {
-        window = {
-          completion = { -- rounded border
-            border = "rounded",
-          },
-          documentation = { -- rounded border
-            border = "rounded",
-          },
-        },
-      })
-    end,
+    -- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
+    opts = {
+      window = {
+        completion = { border = "rounded" },
+        documentation = { border = "rounded" },
+      },
+    },
   },
 }
