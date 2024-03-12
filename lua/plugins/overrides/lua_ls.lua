fe7cf18571b320443328fd592747f80804a408f8
aarondill <aaronsacks2006@gmail.com>
2024-03-12 04:46:13 -0500

    fix: hack: force add neodev types to lua_ls config
    See https://github.com/folke/neodev.nvim/issues/185

--- lua/plugins/overrides/lua_ls.lua
+++ lua/plugins/overrides/lua_ls.lua
@@ -1,3 +1,6 @@
+local library = {}
+--- TODO: remove this when possible!
+if vim.fn.has("nvim-0.10") == 1 then library[#library + 1] = (require("neodev.config").types()) end
 return {
   "neovim/nvim-lspconfig",
   opts = {
@@ -6,6 +9,9 @@ return {
       lua_ls = {
         settings = {
           Lua = {
+            workspace = {
+              library = library,
+            },
             completion = {
               -- use @ to fix a mistake (default)
               postfix = "@",
