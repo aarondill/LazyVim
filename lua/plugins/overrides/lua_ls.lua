eabff16fb1baca6c0968d6254d4d0bc706ab2c18
aarondill <aaronsacks2006@gmail.com>
2024-03-15 04:12:57 -0500

    fix: hack neodev types on LspAttach
    until Neovim types are complete

--- lua/plugins/overrides/lua_ls.lua
+++ lua/plugins/overrides/lua_ls.lua
@@ -1,25 +1,41 @@
-local library = {}
 --- TODO: remove this when possible!
-if vim.fn.has("nvim-0.10") == 1 then library[#library + 1] = (require("neodev.config").types()) end
-return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      ---@type lspconfig.options.lua_ls
-      lua_ls = {
-        settings = {
-          Lua = {
-            workspace = {
-              library = library,
-            },
-            completion = {
-              -- use @ to fix a mistake (default)
-              postfix = "@",
-              callSnippet = "Disable",
-            },
-            hint = {
-              -- Enable in case it starts working in future
-              enable = true,
+vim.api.nvim_create_autocmd("LspAttach", {
+  once = true, -- we don't need to modify the table more than once
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client or client.name ~= "lua_ls" then return end
+    local has_neodev, neodev = pcall(require, "neodev.config")
+    if not has_neodev then return end
+    local neodev_types = assert(neodev.types()) ---@type string
+    ---@type lspconfig.options.lua_ls|vim.lsp.ClientConfig
+    client.config =
+      vim.tbl_deep_extend("keep", client.config, { settings = { Lua = { workspace = { library = {} } } } })
+    --- Already has it :)
+    if vim.tbl_contains(client.config.settings.Lua.workspace.library, neodev_types) then return end
+    table.insert(client.config.settings.Lua.workspace.library, neodev_types)
+    assert(client.notify("workspace/didChangeConfiguration"), "Lsp server has shutdown!")
+  end,
+})
+
+---@type LazySpec
+return { ---@diagnostic disable: missing-fields
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ---@type lspconfig.options.lua_ls
+        lua_ls = {
+          settings = {
+            Lua = {
+              completion = {
+                -- use @ to fix a mistake (default)
+                postfix = "@",
+                callSnippet = "Disable",
+              },
+              hint = {
+                -- Enable in case it starts working in future
+                enable = true,
+              },
             },
           },
         },
